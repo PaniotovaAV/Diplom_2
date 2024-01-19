8f2ca59d0a01b4d5929dfa3f9056b6bef52e7130
Paniotova Alyona <paniotova.av@phystech.edu>
2024-01-19 23:37:22 +0300

    объявила объект класса

--- tests/test_get_orders.py
+++ tests/test_get_orders.py
@@ -28,3 +28,4 @@ class TestGetOrders:
         assert (response_get.status_code == 401 and
                 '"success":false' in response_get.text and
                 response_get.json()['message'] == 'You should be authorised')
+
